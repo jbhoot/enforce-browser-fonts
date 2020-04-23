d41ab023301e3b74076872ccd63724a8fae4b2cd
Jayesh Bhoot <jayesh@bhoot.sh>
2020-04-24 00:15:36 +0530

    restructure project to base on a simple deps.edn+cljs-cli build setup instead of the sprawling shadow-cljs build setup.
    remove the JavaScript version of the source code.
    
    The build scripts have yet to be amended to replace shadow-cljs usage with deps.edn+clj usage

--- build-src-pkg.sh
+++ build-src-pkg.sh
@@ -1,1 +1,1 @@
-zip -r src-pkg.zip build-script.sh manifest.json README-build.md icons cljs-version/src cljs-version/package.json cljs-version/package-lock.json cljs-version/shadow-cljs.
+zip -r src-pkg.zip build-script.sh manifest.json README-build.md icons cljs-version/src cljs-version/package.json cljs-version/package-lock.json cljs-version/shadow-cljs.edn
---
+++ src/ebf/core.cljs
@@ -0,0 +1,64 @@
+(ns ebf.core)
+
+; Third pass
+; new functionality: sync with current state when the addon is installed as well as at every browser startup.
+; more composable, but probably harder to grok at one read?
+(defn new-state [doc-fonts?]
+  (if doc-fonts?
+    {:doc-fonts? true :icon "icons/off.svg" :tooltip "Using Webpage Fonts"}
+    {:doc-fonts? false :icon "icons/on.svg" :tooltip "Using Browser Fonts"}))
+
+(defn set-state! [state]
+  (.all js/Promise [(.set (.-useDocumentFonts (.-browserSettings js/browser)) #js {:value (:doc-fonts? state)})
+                    (.setIcon (.-browserAction js/browser) #js {:path (:icon state)})
+                    (.setTitle (.-browserAction js/browser) #js {:title (:tooltip state)})]))
+
+(defn using-doc-fonts? []
+  (-> (.-useDocumentFonts (.-browserSettings js/browser))
+      (.get #js {})
+      (.then #(.-value %))))
+
+(defn sync-state [invert-curr-state?]
+  (-> (using-doc-fonts?)
+      (.then #(invert-curr-state? %))
+      (.then #(new-state %))
+      (.then #(set-state! %))))
+
+(.addListener (.-onClicked (.-browserAction js/browser)) #(sync-state not))
+(.addListener (.-onInstalled (.-runtime js/browser)) #(sync-state identity))
+(.addListener (.-onStartup (.-runtime js/browser)) #(sync-state identity))
+
+
+; Second pass
+; improved readability through thread-first macro
+; also improved logic
+; (.addListener (.-onClicked (.-browserAction js/browser))
+;   (fn [_]
+;     (->
+;       (.get (.-useDocumentFonts (.-browserSettings js/browser)) #js {})
+;       (.then #(.-value %))
+;       (.then (fn [doc-fonts?]
+;                 (if doc-fonts?
+;                   {:doc-fonts? false :icon "icons/on.svg" :tooltip "Browser Fonts"}
+;                   {:doc-fonts? true :icon "icons/off.svg" :tooltip "Document Fonts"})))
+;       (.then #(.all js/Promise [(.set (.-useDocumentFonts (.-browserSettings js/browser)) #js {:value (:doc-fonts? %)})
+;                                 (.setIcon (.-browserAction js/browser) #js {:path (:icon %)})
+;                                 (.setTitle (.-browserAction js/browser) #js {:title (:tooltip %)})])))))
+
+
+; First pass: one big ball of expressions.
+; Poor readability but its exhilarating to see the
+; 'everything is an expression and returns value' in action
+; as well as to see the whole program as a one big expression.
+; (.addListener (.-onClicked (.-browserAction js/browser))
+;   (fn [_]
+;     (.then
+;       (.then
+;         (.then
+;           (.get (.-useDocumentFonts (.-browserSettings js/browser)) #js {})
+;           #(.-value %))
+;         (fn [doc-fonts?] {:new-fonts (not doc-fonts?) :new-icon (if doc-fonts? "icons/on.svg" "icons/off.svg") :new-tooltip (if doc-fonts? "Browser Fonts" "Document Fonts")}))
+;       #(.all js/Promise [(.set (.-useDocumentFonts (.-browserSettings js/browser)) #js {:value (:new-fonts %)})
+;                                 (.setIcon (.-browserAction js/browser) #js {:path (:new-icon %)})
+;                                 (.setTitle (.-browserAction js/browser) #js {:title (:new-tooltip %)})]))))
+
