8ad3766ff23d2c5e9539ddae28a56b603e5da84f
Jayesh Bhoot <jayesh@bhoot.sh>
2020-05-10 18:06:07 +0530

    Add bootstrap process required to initialize add-on after its installation and after browser startup
    Fix a bug to handle empty object read from storage
    
    Fix a bug to load :default-fonts from storage where its value was loaded as a string instead of a keyword.

--- src/ebf/core.cljs
+++ src/ebf/core.cljs
@@ -3,23 +3,23 @@
 
 (def addon-storage (.-local (.-storage js/browser)))
 
-(def config {:browser-fonts  {:enable  {:pref  {:value false}
-                                        :icon  {:path "icons/on.svg"}
-                                        :title {:title "Using Browser Fonts"}}
-                              :disable {:pref  {:value true}
-                                        :icon  {:path "icons/off.svg"}
-                                        :title {:title "Using Website Fonts"}}}
-             :document-fonts {:enable  {:pref  {:value true}
-                                        :icon  {:path "icons/off.svg"}
-                                        :title {:title "Using Website Fonts"}}
-                              :disable {:pref  {:value false}
-                                        :icon  {:path "icons/on.svg"}
-                                        :title {:title "Using Browser Fonts"}}}})
+(def ui-state {:browser-fonts  {:enable  {:pref  {:value false}
+                                          :icon  {:path "icons/on.svg"}
+                                          :title {:title "Using Browser Fonts"}}
+                                :disable {:pref  {:value true}
+                                          :icon  {:path "icons/off.svg"}
+                                          :title {:title "Using Website Fonts"}}}
+               :document-fonts {:enable  {:pref  {:value true}
+                                          :icon  {:path "icons/off.svg"}
+                                          :title {:title "Using Website Fonts"}}
+                                :disable {:pref  {:value false}
+                                          :icon  {:path "icons/on.svg"}
+                                          :title {:title "Using Browser Fonts"}}}})
 
-(def state (atom {:default-fonts   :browser-fonts
-                  :browser-fonts   {:exclude '()}
-                  :document-fonts  {:exclude '()}
-                  :current-tab-url nil}))
+(def app-state (atom {:default-fonts   :browser-fonts
+                      :browser-fonts   {:exclude '()}
+                      :document-fonts  {:exclude '()}
+                      :current-tab-url nil}))
 
 (defn set-browser-pref! [{pref :pref}]
   (.set (.-useDocumentFonts (.-browserSettings js/browser)) (clj->js pref)))
@@ -35,60 +35,74 @@
                     (set-addon-icon! config)
                     (set-addon-title! config)]))
 
-
-(defn load-from-storage! []
-  (println "loading")
+(defn load-state-from-storage! []
   (-> (.get addon-storage)
       (.then #(js->clj % :keywordize-keys true))
-      (.then #(swap! state merge %))))
+      (.then #(assoc % :default-fonts (keyword (:default-fonts % (clj->js (:default-fonts @app-state))))))
+      (.then #(swap! app-state merge %))))
 
 (defn write-to-storage! [key atom old-state new-state]
-  (println "storing")
   (->> (select-keys new-state [:default-fonts :browser-fonts :document-fonts])
        (clj->js)
        (.set addon-storage)))
 
 (defn configure-addon-for-current-site [key atom old-state new-state]
-  (if (nil? (:current-tab-url new-state))
-    ()
-    (if (some #{(:current-tab-url new-state)} (:exclude ((:default-fonts new-state) new-state)))
-      (configure-addon-for-current-site! (:disable ((:default-fonts new-state) config)))
-      (configure-addon-for-current-site! (:enable ((:default-fonts new-state) config))))))
+  (let [current-domain (:current-tab-url new-state)
+        font-type (:default-fonts new-state)
+        excluded-domains (:exclude (font-type new-state))
+        ui (font-type ui-state)]
+    (if (nil? current-domain)
+      ()
+      (if (some #{current-domain} excluded-domains)
+        (configure-addon-for-current-site! (:disable ui))
+        (configure-addon-for-current-site! (:enable ui))))))
 
 (defn log [key atom old-state new-state]
   (println "old: " old-state)
   (println "new: " new-state))
 
-(add-watch state :log log)
-(add-watch state :write-to-storage! write-to-storage!)
-(add-watch state :configure-addon-for-current-site configure-addon-for-current-site)
-
 (defn domain-name [url] (get (str/split url #"/") 2))
 
 (defn tab-activated [active-info]
   (-> (.get (.-tabs js/browser) (.-tabId active-info))
-      (.then #(swap! state assoc :current-tab-url (domain-name (.-url %))))))
+      (.then #(swap! app-state assoc :current-tab-url (domain-name (.-url %))))))
 
 (defn tab-changed [tab-id change-info tab-info]
   (if (nil? (.-url change-info))
     ()
-    (swap! state assoc :current-tab-url (domain-name (.-url change-info)))))
+    (swap! app-state assoc :current-tab-url (domain-name (.-url change-info)))))
 
 (defn browser-action-activated []
-  (if (some #{(:current-tab-url @state)} (:exclude ((:default-fonts @state) @state)))
-    (swap! state
+  (if (some #{(:current-tab-url @app-state)} (:exclude ((:default-fonts @app-state) @app-state)))
+    (swap! app-state
            update-in
-           [(:default-fonts @state) :exclude]
-           (partial remove #{(:current-tab-url @state)}))
-    (swap! state
+           [(:default-fonts @app-state) :exclude]
+           (partial remove #{(:current-tab-url @app-state)}))
+    (swap! app-state
            update-in
-           [(:default-fonts @state) :exclude]
+           [(:default-fonts @app-state) :exclude]
            conj
-           (:current-tab-url @state))))
+           (:current-tab-url @app-state))))
+
+(defn watch-over-app-state []
+  (add-watch app-state :log log)
+  (add-watch app-state :write-to-storage! write-to-storage!)
+  (add-watch app-state :configure-addon-for-current-site configure-addon-for-current-site))
+
+(defn init []
+  (watch-over-app-state)
+  (load-state-from-storage!)
+  (-> (.query (.-tabs js/browser) (clj->js {:currentWindow true :active true}))
+      (.then #(js->clj % :keywordize-keys true))
+      (.then first)
+      (.then #(:url %))
+      (.then domain-name)
+      (.then #(swap! app-state assoc :current-tab-url %)))
+  )
 
 (.addListener (.-onActivated (.-tabs js/browser)) tab-activated)
 (.addListener (.-onUpdated (.-tabs js/browser)) tab-changed)
 (.addListener (.-onClicked (.-browserAction js/browser)) browser-action-activated)
 
-(.addListener (.-onInstalled (.-runtime js/browser)) load-from-storage!)
-(.addListener (.-onStartup (.-runtime js/browser)) load-from-storage!)
+(.addListener (.-onInstalled (.-runtime js/browser)) init)
+(.addListener (.-onStartup (.-runtime js/browser)) init)
